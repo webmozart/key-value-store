6b6ea8e4724ec3ec8e78b8cc5dd9e79e45a19433
Bernhard Schussek <bschussek@gmail.com>
2014-12-27 16:51:35 +0100

    Added RiakStore

---
+++ src/Impl/RiakStore.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\KeyValueStore\Impl;
+
+use Basho\Riak\Riak;
+use Exception;
+use Webmozart\KeyValueStore\Assert\Assertion;
+use Webmozart\KeyValueStore\InvalidValueException;
+use Webmozart\KeyValueStore\KeyValueStore;
+use Webmozart\KeyValueStore\Purgeable;
+
+/**
+ * A key-value store backed by a Riak client.
+ *
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class RiakStore implements KeyValueStore, Purgeable
+{
+    /**
+     * @var string
+     */
+    private $bucketName;
+
+    /**
+     * @var Riak
+     */
+    private $client;
+
+    /**
+     * Creates a store backed by a Riak client.
+     *
+     * If no client is passed, a new one is created using the default server
+     * "127.0.0.1" and the default port 8098.
+     *
+     * @param string $bucketName The name of the Riak bucket to use.
+     * @param Riak   $client     The client used to connect to Riak.
+     */
+    public function __construct($bucketName, Riak $client = null)
+    {
+        $this->bucketName = $bucketName;
+        $this->client = $client ?: new Riak();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value)
+    {
+        Assertion::key($key);
+
+        try {
+            $serialized = serialize($value);
+        } catch (Exception $e) {
+            throw InvalidValueException::forException($e);
+        }
+
+        $this->client->bucket($this->bucketName)->newObject($key, $serialized)->store();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+    {
+        Assertion::key($key);
+
+        $object = $this->client->bucket($this->bucketName)->get($key);
+
+        if (!$object->exists()) {
+            return $default;
+        }
+
+        return unserialize($object->getData());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($key)
+    {
+        Assertion::key($key);
+
+        $object = $this->client->bucket($this->bucketName)->get($key);
+
+        if (!$object->exists()) {
+            return false;
+        }
+
+        $object->delete();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($key)
+    {
+        Assertion::key($key);
+
+        return $this->client->bucket($this->bucketName)->get($key)->exists();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function purge()
+    {
+        $bucket = $this->client->bucket($this->bucketName);
+
+        foreach ($bucket->getKeys() as $key) {
+            $bucket->get($key)->delete();
+        }
+    }
+}
---
+++ tests/Impl/RiakStoreTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the webmozart/key-value-store package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\KeyValueStore\Tests\Impl;
+
+use Basho\Riak\Riak;
+use Webmozart\KeyValueStore\Impl\RiakStore;
+use Webmozart\KeyValueStore\Tests\AbstractKeyValueStoreTest;
+use Webmozart\KeyValueStore\Tests\PurgeableTestTrait;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class RiakStoreTest extends AbstractKeyValueStoreTest
+{
+    use PurgeableTestTrait;
+
+    private static $supported;
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        $client = new Riak();
+
+        self::$supported = $client->isAlive();
+    }
+
+    protected function setUp()
+    {
+        if (!self::$supported) {
+            $this->markTestSkipped('Riak is not running.');
+        }
+
+        parent::setUp();
+    }
+
+    protected function createStore()
+    {
+        return new RiakStore('test-bucket');
+    }
+}
