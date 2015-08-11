fbb9ef0792a290360bf0b9be1ef21a57bb10dd23
Bernhard Schussek <bschussek@gmail.com>
2015-08-11 16:40:52 +0200

    Partially reverted b664c6ccacfd6eaa3aae647dec030932e4a75bdf

--- src/Api/UnserializationFailedException.php
+++ src/Api/UnserializationFailedException.php
@@ -11,15 +11,51 @@
 
 namespace Webmozart\KeyValueStore\Api;
 
+use Exception;
+use RuntimeException;
+
 /**
  * Thrown when a value cannot be unserialized.
  *
  * @since  1.0
  *
  * @author Bernhard Schussek <bschussek@gmail.com>
- * @author Titouan Galopin <galopintitouan@gmail.com>
  */
-class UnserializationFailedException extends AbstractSerializationException
+class UnserializationFailedException extends RuntimeException
 {
-    protected static $messageTemplate = 'Could not unserialize value of type %s%s';
+    /**
+     * Creates a new exception for the given value.
+     *
+     * @param mixed     $value  The value that could not be unserialized.
+     * @param string    $reason The reason why the value could not be
+     *                          unserialized.
+     * @param int       $code   The exception code.
+     * @param Exception $cause  The exception that caused this exception.
+     *
+     * @return static The new exception.
+     */
+    public static function forValue($value, $reason = '', $code = 0, Exception $cause = null)
+    {
+        return self::forType(is_object($value) ? get_class($value) : gettype($value), $reason, $code, $cause);
+    }
+
+    /**
+     * Creates a new exception for the given value type.
+     *
+     * @param string    $type   The type that could not be unserialized.
+     * @param string    $reason The reason why the value could not be
+     *                          unserialized.
+     * @param int       $code   The exception code.
+     * @param Exception $cause  The exception that caused this exception.
+     *
+     * @return static The new exception.
+     */
+    public static function forType($type, $reason = '', $code = 0, Exception $cause = null)
+    {
+        return new static(sprintf(
+            'Could not unserialize value of type %s%s',
+            $type,
+            $reason ? ': '.$reason : '.'
+        ), $code, $cause);
+    }
 }
