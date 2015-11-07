9e9df90f311647b1e82b62316d61a4c4aecc0469
Bernhard Schussek <bschussek@gmail.com>
2015-11-07 10:27:08 -0600

    Excluded tests that don't work below PHP 5.4

--- tests/JsonFileStoreTest.php
+++ tests/JsonFileStoreTest.php
@@ -240,6 +240,10 @@ class JsonFileStoreTest extends AbstractSortableCountableStoreTest
 
     public function testNoEscapeSlash()
     {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            $this->markTestSkipped('PHP >= 5.4.0 only');
+        }
+
         $store = new JsonFileStore($this->tempDir.'/data.json', JsonFileStore::NO_ESCAPE_SLASH | JsonFileStore::NO_SERIALIZE_STRINGS);
 
         $store->set('foo/', 'bar/');
@@ -260,6 +264,10 @@ class JsonFileStoreTest extends AbstractSortableCountableStoreTest
 
     public function testNoEscapeUnicode()
     {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            $this->markTestSkipped('PHP >= 5.4.0 only');
+        }
+
         $store = new JsonFileStore($this->tempDir.'/data.json', JsonFileStore::NO_ESCAPE_UNICODE | JsonFileStore::NO_SERIALIZE_STRINGS);
 
         $store->set('fooäöü', 'baräöü');
